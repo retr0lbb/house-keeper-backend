0140802d77aab352954b3a42883ec984f47bd425
retr0lbb <retr0lbb@gmail.com>
2025-04-13 15:32:25 -0300

    i dunno what folders i should delete with gitignore

---
+++ src/main/java/com/retr0lbb/housekeeper/HousekeeperApplication.java
@@ -0,0 +1,13 @@
+package com.retr0lbb.housekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousekeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retr0lbb/housekeeper/HousekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retr0lbb.housekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
